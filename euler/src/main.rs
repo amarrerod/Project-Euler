9a2a70bb024fa3a96b311037e1d9e942f13923b0
amarrerod <alemarrerodiaz@gmail.com>
2022-05-03 10:36:28 +0100

    ✨  Solves the first problem

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -1,3 +1,9 @@
+fn multiples_of_3_or_5(limit: i32) -> i32 {
+    let sum = (0..limit).filter(|x| x % 3 == 0 || x % 5 == 0).sum();
+    sum
+}
 fn main() {
     println!("Hello, world!");
+    let result = multiples_of_3_or_5(1000);
+    println!("{}", result);
 }
